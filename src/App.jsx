38e79bf3fa4db2898ca601125ea12bcdd6927239
coffmanjrp <coffmanjrp@gmail.com>
2021-08-20 19:02:41 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="progress-container">
+        <div className="progress"></div>
+        <div className="circle active">1</div>
+        <div className="circle">2</div>
+        <div className="circle">3</div>
+        <div className="circle">4</div>
+      </div>
+      <button type="button" className="btn" disabled>
+        Prev
+      </button>
+      <button type="button" className="btn">
+        Next
+      </button>
     </div>
   );
 }
